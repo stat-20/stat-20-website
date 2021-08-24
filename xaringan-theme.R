66113e92499a49c5b85546e555886b6f90a61b10
andrewpbray <andrewbray@berkeley.edu>
2021-08-23 19:27:43 -0700

    revert font size

--- xaringan-theme.R
+++ xaringan-theme.R
@@ -2,7 +2,7 @@ xaringanthemer::style_mono_accent(
     background_color = "#F3F3EE",
     base_color = "#2F4C3E",
     text_bold_color = "#4A7862",
-    base_font_size = "30px",
+    text_font_size = "1.5rem",
     header_font_google = xaringanthemer::google_font("Bitter", "300"),
     text_font_google   = xaringanthemer::google_font("Commissioner"),
     code_font_google   = xaringanthemer::google_font("Fira Mono")
