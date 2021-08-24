3f3c52bd39f8497fe66c64e5f5177e5f02a55dc3
andrewpbray <andrewbray@berkeley.edu>
2021-08-23 19:00:42 -0700

    change weight

--- xaringan-theme.R
+++ xaringan-theme.R
@@ -1,7 +1,7 @@
 xaringanthemer::style_mono_accent(
     background_color = "#F3F3EE",
     base_color = "#4A7862",
-    header_font_google = xaringanthemer::google_font("Bitter"),
+    header_font_google = xaringanthemer::google_font("Bitter", "300"),
     text_font_google   = xaringanthemer::google_font("Commissioner"),
     code_font_google   = xaringanthemer::google_font("Fira Mono")
 )
