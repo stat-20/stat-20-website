f767a35282da40361d7eb36f5c0d27b220edf3bf
andrewpbray <andrewbray@berkeley.edu>
2021-08-23 18:45:00 -0700

    initial commit

---
+++ xaringan-theme.R
@@ -0,0 +1,6 @@
+xaringanthemer::style_mono_accent(
+    base_color = "#1c5253",
+    header_font_google = xaringanthemer::google_font("Bitter"),
+    text_font_google   = xaringanthemer::google_font("Commissioner"),
+    code_font_google   = xaringanthemer::google_font("Fira Mono")
+)
