c36bb41cb1301b7e72bb56acfd4281df86b8e246
andrewpbray <andrewbray@berkeley.edu>
2021-08-23 19:21:44 -0700

    change colors

--- xaringan-theme.R
+++ xaringan-theme.R
@@ -1,6 +1,7 @@
 xaringanthemer::style_mono_accent(
     background_color = "#F3F3EE",
-    base_color = "#4A7862",
+    base_color = "#2F4C3E",
+    text_bold_color = "#4A7862",
     header_font_google = xaringanthemer::google_font("Bitter", "300"),
     text_font_google   = xaringanthemer::google_font("Commissioner"),
     code_font_google   = xaringanthemer::google_font("Fira Mono")
