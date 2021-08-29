64b7ff725c73c1c65684190bf64e92801c4c3246
andrewpbray <andrewbray@berkeley.edu>
2021-08-29 16:48:56 -0700

    add extra css

--- xaringan-theme.R
+++ xaringan-theme.R
@@ -5,5 +5,13 @@ xaringanthemer::style_mono_accent(
     text_font_size = "1.5rem",
     header_font_google = xaringanthemer::google_font("Bitter", "300"),
     text_font_google   = xaringanthemer::google_font("Commissioner"),
-    code_font_google   = xaringanthemer::google_font("Fira Mono")
+    code_font_google   = xaringanthemer::google_font("Fira Mono"),
+    extra_css = list(".pull-left-wide" = list("width" = "70%",
+                                              "float" = "left"),
+                     ".pull-right-narrow" = list("width" = "27%",
+                                                 "float" = "right"),
+                     ".pull-left-narrow" = list("width" = "27%",
+                                                "float" = "left"),
+                     ".pull-right-wide" = list("width" = "70%",
+                                               "float" = "right"))
 )
