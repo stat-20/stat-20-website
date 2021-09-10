95f5d8304d50343ee8a6d87b1b493065ff6b7ba7
andrewpbray <andrewbray@berkeley.edu>
2021-09-10 13:49:47 -0700

    name colors

--- xaringan-theme.R
+++ xaringan-theme.R
@@ -1,12 +1,20 @@
+# color palette
+warm_white <- "#F3F3EE"
+forest_green <- "#2F4C3E"
+kelly_green <- "#4A7862"
+yellow <- "#F1DE67"
+
+# set ggplot2 theme
 theme_stat20 <- theme_gray() +
-    theme(plot.background = element_rect(fill = "#F3F3EE",
+    theme(plot.background = element_rect(fill = warm_white,
                                          colour = NA))
 ggplot2::theme_set(theme_stat20)
 
+# set xaringan theme
 xaringanthemer::style_mono_accent(
-    background_color = "#F3F3EE",
-    base_color = "#2F4C3E",
-    text_bold_color = "#4A7862",
+    background_color = "warm_white",
+    base_color = "forest_green",
+    text_bold_color = "kelly_green",
     text_font_size = "1.5rem",
     header_font_google = xaringanthemer::google_font("Bitter", "300"),
     text_font_google   = xaringanthemer::google_font("Commissioner"),
@@ -30,6 +38,6 @@ xaringanthemer::style_mono_accent(
                                     "padding-bottom"   = "3px",
                                     "margin-bottom"    = "6px",
                                     "margin-top"       = "6px",
-                                    "border-left"      = "solid 5px #F1DE67",
-                                    "background-color" = "#F1DE6750"))
+                                    "border-left"      = paste("solid", "5px", yellow),
+                                    "background-color" = paste0(yellow, 50)))
 )
